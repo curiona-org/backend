2ce8c3bb722a2bb40a4349f45bf5a997756e83fc
HotPotatoC <juandotulung@gmail.com>
2025-02-12 15:31:47 +0700

    test(config): removed comment

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -39,7 +39,6 @@ func TestConfig_Init(t *testing.T) {
 	setEnv("REFRESH_EXPIRES_IN", "720h")
 	setEnv("LLM_API_KEY", "test_api_key")
 	setEnv("LLM_MODEL", "gpt-4")
-	// Added missing configurations
 	setEnv("GOOGLE_CLIENT_ID", "test_google_id")
 	setEnv("GOOGLE_CLIENT_SECRET", "test_google_secret")
 	setEnv("OTLP_EXPORTER_ENDPOINT", "http://localhost:4317")
