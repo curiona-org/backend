96ba86142095ea20b9ad9d2baea240ec038de701
HotPotatoC <juandotulung@gmail.com>
2025-02-16 19:42:13 +0700

    feat: added tracing for noop cache

--- pkg/cache/noop.go
+++ pkg/cache/noop.go
@@ -1,22 +1,40 @@
 package cache
 
-import "context"
+import (
+	"context"
 
-type noopCache[V any] struct{}
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+)
 
-func NewNoopCache[V any]() Cache[V] {
-	return &noopCache[V]{}
+type noopCache[V any] struct {
+	tracer trace.Tracer
 }
 
-func (*noopCache[V]) Get(ctx context.Context, key string) (value V, ok bool) {
+func NewNoopCache[V any]() Cache[V] {
+	tracer := otel.Tracer("cache:noop")
+	return &noopCache[V]{tracer: tracer}
+}
+
+func (c *noopCache[V]) Get(ctx context.Context, key string) (value V, ok bool) {
+	ctx, span := c.tracer.Start(ctx, "(*noopCache[V]).Get")
+	defer span.End()
 	return
 }
 
-func (*noopCache[V]) Set(ctx context.Context, key string, value V) {}
+func (c *noopCache[V]) Set(ctx context.Context, key string, value V) {
+	ctx, span := c.tracer.Start(ctx, "(*noopCache[V]).Set")
+	defer span.End()
+	return
+}
 
-func (*noopCache[V]) Delete(ctx context.Context, key ...string) error {
+func (c *noopCache[V]) Delete(ctx context.Context, key ...string) error {
+	ctx, span := c.tracer.Start(ctx, "(*noopCache[V]).Delete")
+	defer span.End()
 	return nil
 }
-func (*noopCache[V]) Truncate(ctx context.Context) error {
+func (c *noopCache[V]) Truncate(ctx context.Context) error {
+	ctx, span := c.tracer.Start(ctx, "(*noopCache[V]).Truncate")
+	defer span.End()
 	return nil
 }
