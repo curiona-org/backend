f0a315e20ceb9c97fa89517ba509117d2acd01a8
HotPotatoC <juandotulung@gmail.com>
2025-02-19 20:03:46 +0700

    build(lint): removed unused nolint revive directive

--- internal/app/auth_verify.go
+++ internal/app/auth_verify.go
@@ -6,6 +6,6 @@ import (
 	"github.com/roadmap-thesis/backend/internal/auth"
 )
 
-func (app *application) AuthVerify(ctx context.Context, token string) (*auth.Payload, error) { //nolint:revive
+func (app *application) AuthVerify(ctx context.Context, token string) (*auth.Payload, error) {
 	return app.auth.Access.Parse(token)
 }
