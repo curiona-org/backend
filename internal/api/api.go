1ba984ad94b0f33eca779cbcbd5680371e10b669
HotPotatoC <juandotulung@gmail.com>
2025-02-24 20:51:10 +0700

    feat(api): added back rate limiting on production setting

--- internal/api/api.go
+++ internal/api/api.go
@@ -13,6 +13,7 @@ import (
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog/log"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	"golang.org/x/time/rate"
 )
 
 type API struct {
@@ -91,8 +92,8 @@ func (a *API) setupMiddlewares() {
 			return nil
 		},
 	}))
-	// if config.IsProduction() {
-	// 	a.instance.Use(echoMiddleware.RateLimiter(echoMiddleware.NewRateLimiterMemoryStore(rate.Limit(20))))
-	// }
+	if config.IsProduction() {
+		a.instance.Use(echoMiddleware.RateLimiter(echoMiddleware.NewRateLimiterMemoryStore(rate.Limit(20))))
+	}
 	a.instance.Use(otelecho.Middleware("api"))
 }
