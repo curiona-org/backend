166f3d40a1b09e052a3dc44db70673bb34ca6467
HotPotatoC <juandotulung@gmail.com>
2025-02-24 14:48:33 +0700

    test(k6): added ramping arrival rate scenario

--- load-test/src/health.ts
+++ load-test/src/health.ts
@@ -7,6 +7,20 @@ export const options: Options = {
     http_req_failed: ["rate<0.01"],
     http_req_duration: ["p(99)<10"],
   },
+  scenarios: {
+    ramping_arrival_rate: {
+      executor: 'ramping-arrival-rate',
+      startRate: 1000, // 1000 RPS
+      timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
+      stages: [
+        { target: 1000, duration: '10s' },
+        { target: 10000, duration: '20s' },
+        { target: 1000, duration: '10s' },
+      ],
+      preAllocatedVUs: 100, // how large the initial pool of VUs would be
+      maxVUs: 200,
+    }
+  }
 };
 
 export default function () {
--- load-test/src/register.ts
+++ load-test/src/register.ts
@@ -1,24 +1,37 @@
-import { check, fail } from "k6";
+import { check, fail, randomSeed } from "k6";
 import exec from "k6/execution";
 import http from "k6/http";
 import { Counter } from "k6/metrics";
 import { Options } from "k6/options";
-
 export const options: Options = {
   thresholds: {
     http_req_failed: ["rate<0.01"],
-    http_req_duration: ["p(99)<1000"],
+    http_req_duration: ["p(99)<1500"],
+  },
+  scenarios: {
+    ramping_arrival_rate: {
+      executor: "ramping-arrival-rate",
+      startRate: 1000, // 1000 RPS
+      timeUnit: "1s", // 1000 iterations per second, i.e. 1000 RPS
+      stages: [
+        { target: 1000, duration: "10s" },
+        { target: 10000, duration: "20s" },
+        { target: 1000, duration: "10s" },
+      ],
+      preAllocatedVUs: 100, // how large the initial pool of VUs would be
+      maxVUs: 300,
+    },
   },
 };
 
-const usersCreated = new Counter("users_created");
-const usersLoggedIn = new Counter("users_logged_in");
+const accountsRegisteredCounter = new Counter("accounts_registered");
 
 export default function () {
-  const uid = exec.scenario.iterationInTest;
+  randomSeed(Date.now());
+  const uid = `${Math.random().toString()}${exec.scenario.iterationInTest}`;
   const body = {
-    name: `User ${uid}`,
-    email: `testuser${uid}@example.com`,
+    name: `${uid}`,
+    email: `${uid}@example.com`,
     password: "123123",
   };
 
@@ -36,14 +49,10 @@ export default function () {
   check(registration, {
     "user registration successful": (r) =>
       [200, 201].includes(r.status) ||
-      fail(`Registration failed for ${body.name} ${body.email}`),
+      fail(`${r.status} Registration failed for ${body.name} ${body.email}: ${r.body}`),
   });
 
-  if (registration.status === 200) {
-    usersLoggedIn.add(1);
-  }
-
   if (registration.status === 201) {
-    usersCreated.add(1);
+    accountsRegisteredCounter.add(1);
   }
 }
